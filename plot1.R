af88dc57abc90ad3b506398f38b911ec8edc1c31
rhammer35 <rhammer35@icloud.com>
2019-07-30 22:30:30 -0400

    Added explanation for one of the code lines

--- plot1.R
+++ plot1.R
@@ -2,8 +2,10 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## Use tapply to sum the pm25 emissions for each year in the data set and store the values
+## Use tapply to sum the pm2.5 emissions for each year in the data set and store the values
 pm25_totals <- tapply(NEI$Emissions, NEI$year, sum)
+
+## Convert pm2.5 emission totals data from tons to kilotons for easier graph reading
 pm25_totals <- pm25_totals/1000
 
 ## Initialize png plotting device
