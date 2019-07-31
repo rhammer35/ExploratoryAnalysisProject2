bc4a60320a44f5e09b1a8d15d10103ee3b3dd1b1
rhammer35 <rhammer35@icloud.com>
2019-07-30 22:43:45 -0400

    Made code structure cleaner

--- plot1.R
+++ plot1.R
@@ -2,18 +2,19 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## Use tapply to sum the pm2.5 emissions for each year in the data set and store the values
+## Use tapply to sum the pm2.5 emissions for each year in the data set and store
+## the values
 pm25_totals <- tapply(NEI$Emissions, NEI$year, sum)
 
-## Convert pm2.5 emission totals data from tons to kilotons for easier graph reading
+## Convert pm2.5 emission totals from tons to kilotons for easier graph reading
 pm25_totals <- pm25_totals/1000
 
 ## Initialize png plotting device
 png(filename = "plot1.png")
 
 ## Create line graph in base plotting system showing total emissions over time
-plot(unique(NEI$year), pm25_totals, type = "l", ylab = "Total Emissions (in kilotons)", xlab = "",
-     main = "pm2.5 emissions in the US over time")
+plot(unique(NEI$year), pm25_totals, type = "l", ylab = "Total Emissions
+     (in kilotons)", xlab = "", main = "pm2.5 emissions in the US over time")
 
 ## End plotting device
 dev.off()
