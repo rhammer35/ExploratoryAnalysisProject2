f962910611354d7fa27f7f0b2de0316c341d6747
rhammer35 <rhammer35@icloud.com>
2019-07-30 22:26:14 -0400

    Initial code for creation of plot 1

---
+++ plot1.R
@@ -0,0 +1,17 @@
+## Reads data into data frame from intial txt file in working directory
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Use tapply to sum the pm25 emissions for each year in the data set and store the values
+pm25_totals <- tapply(NEI$Emissions, NEI$year, sum)
+pm25_totals <- pm25_totals/1000
+
+## Initialize png plotting device
+png(filename = "plot1.png")
+
+## Create line graph in base plotting system showing total emissions over time
+plot(unique(NEI$year), pm25_totals, type = "l", ylab = "Total Emissions (in kilotons)", xlab = "",
+     main = "pm2.5 emissions in the US over time")
+
+## End plotting device
+dev.off()
