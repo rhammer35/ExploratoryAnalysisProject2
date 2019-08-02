502eaf3f059314a169e6100abc519c0bf3b6737e
rhammer35 <rhammer35@icloud.com>
2019-08-02 14:52:41 -0400

    Removed unneeded call to ggplot2

--- plot5.R
+++ plot5.R
@@ -2,9 +2,6 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## Load ggplot2
-library(ggplot2)
-
 ## Subset NEI data into data frame containing only Baltimore City data
 NEI_Baltimore_City <- subset(NEI, fips == "24510")
 
