16f000d36f516c22d6d90ae8d890ec0812842ee1
rhammer35 <rhammer35@icloud.com>
2019-08-02 10:19:58 -0400

    Added code to subset SCC data to include only types of coal combustion and then subsetted NEI data on those types

--- plot4.R
+++ plot4.R
@@ -2,3 +2,21 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+## Subset SCC data to sources that could use coal combustion
+## This includes sources with "combustion" in the title
+SCC_Combustion <- SCC[grep("Combustion", SCC$SCC.Level.One), ]
+
+## Now further subset to include only sources listing "Coal" or "Lignite"
+## in level three (perusing the data showed lignite in level three 
+## corresponded to a type of coal in level four)
+SCC_Coal <- SCC_Combustion[grep("Coal", SCC_Combustion$SCC.Level.Three), ]
+SCC_Lignite <- SCC_Combustion[grep("Lignite", 
+                                   SCC_Combustion$SCC.Level.Three), ]
+
+## Merge lignite and coal data and remove any overlapping rows
+SCC_Coal_Combustion <- merge(SCC_Coal, SCC_Lignite, all = TRUE)
+
+## Subset NEI data to only those SCC types corresponding to coal combustion
+SCC_Combustion_Types <- as.character(unique(SCC_Coal_Combustion$SCC))
+NEI_Coal_Combustion <- NEI[NEI$SCC %in% SCC_Combustion_Types, ]
+
