5eca1cd9f1c1506a8bbe09acb91721592167166d
rhammer35 <rhammer35@icloud.com>
2019-08-02 13:58:54 -0400

    Subset SCC data into only motor vehicle types and subsetted NEI Baltimore City data using vehicle types in subsetted SCC data

--- plot5.R
+++ plot5.R
@@ -7,3 +7,15 @@ library(ggplot2)
 
 ## Subset NEI data into data frame containing only Baltimore City data
 NEI_Baltimore_City <- subset(NEI, fips == "24510")
+
+## Subset SCC data into data frame containing only motor vehicle sources
+## by sorting Short.Name variable using phrase "Highway Veh"
+## It should be noted that I am choosing to interpret "motor vehicle" as
+## referring to only motorized vehicles driven on roadways
+SCC_Motor_Vehicle <- SCC[grep("Highway Veh", SCC$Short.Name), ]
+
+## Subset Baltimore Citydata to only those SCC types corresponding to 
+## motor vehicles
+SCC_Vehicle_Types <- as.character(unique(SCC_Motor_Vehicle$SCC))
+NEI_Baltimore_Vehicles <- NEI_Baltimore_City[NEI_Baltimore_City$SCC %in% 
+                                             SCC_Vehicle_Types, ]
