6f6ada612c9fce6cf574345ab7f3f5bd62240f9c
rhammer35 <rhammer35@icloud.com>
2019-08-02 14:14:20 -0400

    Calculated number of emissions sources, total emissions, mean emissions, and max emission for each year of NEI Baltimore motor vehicle subset and created graph over time of each in 2x2 panel

--- plot5.R
+++ plot5.R
@@ -14,8 +14,43 @@ NEI_Baltimore_City <- subset(NEI, fips == "24510")
 ## referring to only motorized vehicles driven on roadways
 SCC_Motor_Vehicle <- SCC[grep("Highway Veh", SCC$Short.Name), ]
 
-## Subset Baltimore Citydata to only those SCC types corresponding to 
+## Subset Baltimore City data to only those SCC types corresponding to 
 ## motor vehicles
 SCC_Vehicle_Types <- as.character(unique(SCC_Motor_Vehicle$SCC))
 NEI_Baltimore_Vehicles <- NEI_Baltimore_City[NEI_Baltimore_City$SCC %in% 
                                              SCC_Vehicle_Types, ]
+
+## Calculating total number of sources measured per year in NEI data subset
+Bmore_vehicle_sources <- tapply(NEI_Baltimore_Vehicles$SCC,
+                                NEI_Baltimore_Vehicles$year, length)
+
+## Taking yearly totals for emissions in NEI Baltimore motor vehicle subset
+pm25_Bmore_vehicle_total <- tapply(NEI_Baltimore_Vehicles$Emissions,
+                                   NEI_Baltimore_Vehicles$year, sum)
+
+## Calculating yearly emission averages in NEI Baltimore motor vehicle subset
+pm25_Bmore_vehicle_avg <- tapply(NEI_Baltimore_Vehicles$Emissions,
+                                 NEI_Baltimore_Vehicles$year, mean)
+
+## Calculating max yearly emission in NEI Baltimore motor vehicle subset
+pm25_Bmore_vehicle_max <- tapply(NEI_Baltimore_Vehicles$Emissions,
+                                 NEI_Baltimore_Vehicles$year, max)
+
+## Initialize png plotting device
+png(filename = "plot5.png")
+
+## Create 2x2 panel plot in base plotting system containing line graphs of
+## each data component calculated off of NEI Baltimore vehicle subset
+par(mfrow = c(2, 2), mar = c(5, 4, 2, 2), oma = c(0, 0, 2, 0))
+plot(unique(NEI_Baltimore_Vehicles$year), pm25_Bmore_vehicle_total, type = "l",
+     ylab = "Total Emissions (in tons)", xlab = "")
+plot(unique(NEI_Baltimore_Vehicles$year$year), pm25_Bmore_vehicle_avg,
+     type = "l", ylab = "Average Emissions (in tons)", xlab = "")
+plot(unique(NEI_Baltimore_Vehicles$year$year), pm25_Bmore_vehicle_max,
+     type = "l", ylab = "Maximum Measured Emission (in tons)", xlab = "")
+plot(unique(NEI_Baltimore_Vehicles$year$year), Bmore_Vehicle_Sources,
+     type = "l", ylab = "Number of Coal Emission Sources", xlab = "")
+mtext("pm2.5 motor vehicle emissions in Baltimore City", side = 3, outer = TRUE)
+
+## End plotting device
+dev.off()
